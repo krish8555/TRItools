4f4fe9b397882807fbe358b21231d3f47a262a47
KRISH PATEL <kalpeshkrish.22.cse@iite.indusuni.ac.in>
2024-08-29 16:54:11 +0530

    Qr done without header and footer
    minor changes yet to be done

---
+++ QR/qr.js
@@ -0,0 +1,14 @@
+let imgBox = document.getElementById("imgBox");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+
+function generateQRCode() {
+  if (qrText.value === "") {
+    alert("Please enter the text to generate QR Code");
+    return;
+  }
+  qrImage.src =
+    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
+    qrText.value;
+  imgBox.classList.add("show-img");
+}
