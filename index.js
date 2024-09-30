d9317c6edf5c0f51e8e3a15768c2fa0cba37099d
KRISH PATEL <kalpeshkrish.22.cse@iite.indusuni.ac.in>
2024-09-30 21:20:21 +0530

    main page

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("qr-btn").onclick = function () {
+  window.location.href = "../QR/qr.html"; // Link to QR Code Generator Page
+};
+
+document.getElementById("pwd-btn").onclick = function () {
+  window.location.href = "../Pass/pass.html";
+};
+
+document.getElementById("todo-btn").onclick = function () {
+  window.location.href = "../todo/todo.html";
+};
