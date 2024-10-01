11bc4e945fd32a1dcae876931c3be660abe0158e
KRISH PATEL <137062224+krish8555@users.noreply.github.com>
2024-10-01 13:42:42 +0530

    link correction

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 document.getElementById("qr-btn").onclick = function () {
-  window.location.href = "../QR/qr.html"; // Link to QR Code Generator Page
+  window.location.href = "..TRITTOLS/QR/qr.html"; // Link to QR Code Generator Page
 };
 
 document.getElementById("pwd-btn").onclick = function () {
-  window.location.href = "../Pass/pass.html";
+  window.location.href = "..TRITTOLS/Pass/pass.html";
 };
 
 document.getElementById("todo-btn").onclick = function () {
-  window.location.href = "../todo/todo.html";
+  window.location.href = "..TRITTOLS/todo/todo.html";
 };
